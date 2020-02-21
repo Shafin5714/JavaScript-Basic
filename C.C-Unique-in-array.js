f4e8c3f681232daef3d9c5de06cd98d846ed3a49
Shafin5714 <shafin5714@gmail.com>
2020-02-21 17:34:25 +0600

    using concat

--- C.C-Unique-in-array.js
+++ C.C-Unique-in-array.js
@@ -1,17 +1,29 @@
 function diffArray(arr1,arr2){
-    var result = []
-    for(var i=0;i<arr1.length;i++){
-       if(arr2.indexOf(arr1[i])===-1){
-        result.push(arr1[i])
-       }
-    }
-    for(var j=0;j<arr2.length;j++){
-        if(arr1.indexOf(arr2[j])===-1){
-            result.push(arr2[j])
-        }
-    }
+    // var result = []
+    // for(var i=0;i<arr1.length;i++){
+    //    if(arr2.indexOf(arr1[i])===-1){
+    //     result.push(arr1[i])
+    //    }
+    // }
+    // for(var j=0;j<arr2.length;j++){
+    //     if(arr1.indexOf(arr2[j])===-1){
+    //         result.push(arr2[j])
+    //     }
+    // }
     
-    return result
+    // return result
+
+
+    // first combine to array
+    let combine = arr1.concat(arr2)
+    console.log(combine);
+
+   return combine.filter((num)=>{
+        if(arr1.indexOf(num)===-1||arr2.indexOf(num)===-1){
+            return num
+        }
+    })
+    
 }
 
 console.log(diffArray([1,2,3,5],[1,2,3,5,4])); // 4 unique in array
